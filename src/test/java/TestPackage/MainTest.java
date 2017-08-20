a9c2496e02dcfcff7567d6e33a25e63eb00c0ae6
Andrii Gubarenko <Andrewgubarenko@gmail.com>
2017-08-20 23:12:24 +0300

    First commit

---
+++ src/test/java/TestPackage/MainTest.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestPackage;
+
+import com.andriigubarenko.beatbox.BeatBoxUserSide;
+
+/**
+ *
+ * @author Андрей
+ */
+public class MainTest {
+    public static void main (String[] args) {
+        new BeatBoxUserSide().startUp("1");
+      }
+}
